a227fb603087c0642d9e0ec539bafa8d5eea7970
¨roniekun¨ <roniebenitez01@gmail.com>
2024-08-14 20:35:24 +0800

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,24 +1,23 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+// import NextAuth from 'next-auth'
+// import GoogleProvider from 'next-auth/providers/google'
 
-const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clinentId: '', //provide
-            clientSecret: '', //provide
-        })
-    ],
-    
-    callbacks:{
-        async session({session}){
+// const handler = NextAuth({
+//     providers:[
+//         GoogleProvider({
+//             clinentId: '', //provide
+//             clientSecret: '', //provide
+//         })
+//     ],
 
-},
-        async signIn({profile}){
-    
-}
-    }
+//     callbacks:{
+//         async session({session}){
 
-})
+// },
+//         async signIn({profile}){
 
-export {handler as GET, handler as POST};
+// }
+//     }
 
+// })
+
+// export {handler as GET, handler as POST};
