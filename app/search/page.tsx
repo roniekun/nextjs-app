0f99d3e641535f6b912ce86fda6d4a873a34daf7
¨roniekun¨ <roniebenitez01@gmail.com>
2024-09-13 15:45:11 +0800

    update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,10 +1,13 @@
 import SearchResult from "@/components/utils/search-result";
 import Container from "@/components/utils/container";
+import { Suspense } from "react";
 
 export default function Search() {
   return (
     <Container className="min-h-screen flex place-content-center flex-col w-full">
-      <SearchResult />
+      <Suspense>
+        <SearchResult />
+      </Suspense>
     </Container>
   );
 }
