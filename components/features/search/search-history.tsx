db81489cdaeb0bc75a3c89d137fd89288b547e46
¨roniekun¨ <roniebenitez01@gmail.com>
2024-09-17 22:53:57 +0800

    update

--- components/features/search/search-history.tsx
+++ components/features/search/search-history.tsx
@@ -5,7 +5,6 @@ import { useSearch } from "@/provider/context/SearchContext";
 import { useRouter } from "next/navigation";
 import { SearchHistoryProps } from "@/provider/context/SearchContext";
 import UpdateOutlinedIcon from "@mui/icons-material/UpdateOutlined";
-import filterSearchItems from "./utils/filterSearchItems";
 
 type Props = {
   filteredSearchItems: SearchHistoryProps[];
@@ -13,56 +12,50 @@ type Props = {
     React.SetStateAction<SearchHistoryProps[]>
   >;
 };
-
 const SearchHistoryModal: React.FC<Props> = ({
   filteredSearchItems,
   setFilteredSearchItems,
 }) => {
   const router = useRouter();
   const { searchItems, setSearchItems, setQuery } = useSearch();
-  const historyRef = useRef<HTMLLIElement[]>([]);
-  const [deletedItem, setDeletedItem] = useState<SearchHistoryProps>();
+  const historyRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const [deletedItem, setDeletedItem] = useState<SearchHistoryProps | null>(
+    null
+  );
 
   const setRef = (el: HTMLLIElement | null, idx: number) => {
     if (el) {
-      historyRef.current[idx] = el;
+      historyRefs.current[idx] = el;
     }
   };
 
   const handleClick = (idx: number) => {
-    setQuery(historyRef.current[idx].textContent);
-    router.replace(
-      `/search?query=${encodeURIComponent(
-        historyRef.current[idx].textContent ?? ""
-      )}`
-    );
+    const textContent = historyRefs.current[idx]?.textContent ?? "";
+    setQuery(textContent);
+    router.replace(`/search?query=${encodeURIComponent(textContent)}`);
   };
 
   //deleting items in history
   const handleDelete = (id: number) => {
-    const deletedItem = filteredSearchItems.filter((item) => item.id === id); //storing the  search history user deleted
-    const updatedItems = searchItems.filter((searchItems) =>
-      deletedItem.some((item) => item.id !== searchItems.id)
-    ); // updating the original search collection
-    const targetItem = deletedItem[0];
-    const updatedFilteredSearchItems = filteredSearchItems.filter(
-      (item) => item.id !== targetItem.id
-    );
-    setFilteredSearchItems(updatedFilteredSearchItems);
-    setSearchItems(updatedItems);
-    setDeletedItem(deletedItem[0]);
+    const deletedItem = filteredSearchItems.find((item) => item.id === id); //storing the  search history user deleted
+    if (deletedItem) {
+      const updatedItems = searchItems.filter(
+        (item) => deletedItem.id !== item.id
+      ); // updating the original search collection
+      setSearchItems(updatedItems);
+      setDeletedItem(deletedItem);
+    }
   };
 
-  // useEffect(() => {
-  //   console.log(deletedItem);
-  //   if (deletedItem) {
-  //     const updatedFilteredSearchItems = filteredSearchItems.filter(
-  //       (item) => item.id !== deletedItem.id
-  //     );
+  useEffect(() => {
+    if (deletedItem) {
+      const updatedFilteredSearchItems = filteredSearchItems.filter(
+        (item) => !item.search.includes(deletedItem.search)
+      );
 
-  //     setFilteredSearchItems(updatedFilteredSearchItems);
-  //   }
-  // }, [deletedItem]);
+      setFilteredSearchItems(updatedFilteredSearchItems);
+    }
+  }, [deletedItem, filteredSearchItems, setFilteredSearchItems]);
 
   return (
     <ul className="relative flex flex-col w-full rounded-b-md h-auto text-[--text-color-secondary]">
@@ -70,12 +63,13 @@ const SearchHistoryModal: React.FC<Props> = ({
         <li
           key={idx}
           ref={(el) => setRef(el, idx)}
-          className="flex list-none w-full relative rounded-sm hover:bg-neutral-200 hover:bg-opacity-50 justify-between gap-x-1 "
+          className="flex list-none w-full relative rounded-sm justify-between gap-x-1 "
         >
           <UpdateOutlinedIcon />
           <a
             onClick={() => handleClick(idx)}
-            className="flex-1 cursor-pointer flex item-center relative gap-x-1 text-[--text-color-secondary] "
+            className="flex-1 cursor-pointer  hover:bg-neutral-200 hover:bg-opacity-50 flex item-center relative gap-x-1 
+            text-[--text-color-secondary] "
           >
             {item.search}
           </a>
