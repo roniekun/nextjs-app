7600dde492ee93e08252ad216ade845f85b226ec
¨roniekun¨ <roniebenitez01@gmail.com>
2024-10-16 12:49:24 +0800

    update

--- components/layout/Banner.tsx
+++ components/layout/Banner.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import TextSlider from "../lib/ui/text-slider";
 import ToggleTheme from "../lib/ui/toggle-theme";
-import Container from "../lib/ui/container";
 
 const Banner = () => {
   return (
-    <div className="w-screen top-0 relative flex">
-      <Container>
-        <h3 className="text-sm text-green-600 font-bold">Active now</h3>
-        <TextSlider title=" - Available for freelance work" />
-        <ToggleTheme />
-      </Container>
+    <div className="w-screen relative flex px-[5vw] justify-between h-fit">
+      <h3 className="text-sm text-green-600 font-bold">Active now</h3>
+      <TextSlider title=" - Available for freelance work - Available for freelance work - Available for freelance work" />
+      <ToggleTheme />
     </div>
   );
 };
