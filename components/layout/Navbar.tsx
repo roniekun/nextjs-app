5e99264db34c942fa1d0b6e97c07fd560f20a86c
¨roniekun¨ <roniebenitez01@gmail.com>
2024-10-16 10:32:18 +0800

    update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -27,15 +27,14 @@ export default function Navbar() {
       gsap.to(".navbar", {
         display: "flex",
         onComplete: () => {
-          gsap.to(".container", {
+          gsap.to(".navbar", {
             opacity: 1,
-            duration: 0.3,
             ease: "power2.out", //https://easings.net/#easeInOutQuart,
           });
         },
       });
     } else {
-      gsap.to(".container", {
+      gsap.to(".navbar", {
         opacity: 0,
         duration: 0.3,
         ease: "power2.out",
@@ -49,47 +48,45 @@ export default function Navbar() {
   }, [isOpenSearch, isToggleMenu, navRef]);
 
   return (
-    <PageTransitionLayout>
-      <nav
-        ref={navRef}
-        className={`${
-          theme === "dark"
-            ? "bg-[--background-dark] text-neutral-200"
-            : "bg-[--background-light] text-neutral-900"
-        } navbar justify-between items-start overflow-clip hidden fixed h-[100vh] left-0 top-0 w-screen z-10`}
-      >
-        <div className="container opacity-0">
-          {isToggleMenu && (
-            <div
-              className={`mt-[--header-height] lg:max-w-7xl p-[5vw]  w-full relative flex flex-col h-fit gap-y-5`}
-            >
-              <div>
-                <h1 className=" font-medium uppercase my-1">Navigations</h1>
-                <ul>
-                  <Links className="text-3xl font-medium" />
-                </ul>
-              </div>
-              <div className="flex flex-col flex-1 ">
-                <h1 className="w-full relative uppercase font-medium my-1">
-                  Socials
-                </h1>
-                <ul>
-                  <Social className="text-3xl" />
-                </ul>
-              </div>
-              <div className="flex flex-col flex-1">
-                <ToggleTheme />
-              </div>
+    <nav
+      ref={navRef}
+      className={`${
+        theme === "dark"
+          ? "bg-[--background-dark] text-neutral-200"
+          : "bg-[--background-light] text-neutral-900"
+      } navbar justify-between items-start overflow-clip hidden fixed h-[100vh] left-0 top-0 w-screen z-10`}
+    >
+      <PageTransitionLayout>
+        {isToggleMenu && (
+          <div
+            className={`mt-[--header-height] lg:max-w-7xl p-[5vw]  w-full relative flex flex-col h-fit gap-y-5`}
+          >
+            <div>
+              <h1 className=" font-medium uppercase my-1">Navigations</h1>
+              <ul>
+                <Links className="text-3xl font-medium" />
+              </ul>
             </div>
-          )}
+            <div className="flex flex-col flex-1 ">
+              <h1 className="w-full relative uppercase font-medium my-1">
+                Socials
+              </h1>
+              <ul>
+                <Social className="text-3xl font-medium" />
+              </ul>
+            </div>
+            <div className="flex flex-col flex-1">
+              <ToggleTheme />
+            </div>
+          </div>
+        )}
 
-          {isOpenSearch && (
-            <div className="mt-[--header-height] ">
-              <Search placeholder="Search..." contentData={contentData} />
-            </div>
-          )}
-        </div>
-      </nav>
-    </PageTransitionLayout>
+        {isOpenSearch && (
+          <div className="mt-[--header-height] ">
+            <Search placeholder="Search..." contentData={contentData} />
+          </div>
+        )}
+      </PageTransitionLayout>
+    </nav>
   );
 }
