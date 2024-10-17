fc3915a05494dcea3d5b29b4487617251f046440
¨roniekun¨ <roniebenitez01@gmail.com>
2024-10-18 00:16:47 +0800

    update

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -15,7 +15,7 @@ const Menu = () => {
   return (
     <div style={{ fontFamily: "Neue Bit , Mori" }}>
       <button
-        className="capitalize h-8 w-16 text-sm flex justify-center items-center bg-neutral-500 bg-opacity-15 overflow-hidden hover:border border-none border-neutral-500
+        className="capitalize relative h-8 w-16 text-sm flex justify-center items-center bg-neutral-500 bg-opacity-15 overflow-hidden hover:border border-none border-neutral-500
          rounded-md hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.7)] transition-shadow duration-300 z-10"
         onClick={handleClick}
       >
@@ -23,10 +23,10 @@ const Menu = () => {
           <AnimatePresence>
             {isToggleMenu && (
               <motion.p
-                initial={{ y: -20, opacity: 0 }}
+                initial={{ y: -20 }}
                 transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 20, opacity: 0 }}
+                animate={{ y: 0 }}
+                exit={{ y: 20 }}
                 className="absolute"
               >
                 Close
@@ -36,10 +36,10 @@ const Menu = () => {
           <AnimatePresence initial={false}>
             {!isToggleMenu && (
               <motion.p
-                initial={{ y: -20, opacity: 0 }}
+                initial={{ y: -20 }}
                 transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 20, opacity: 0 }}
+                animate={{ y: 0 }}
+                exit={{ y: 20 }}
                 className="absolute"
               >
                 Menu
