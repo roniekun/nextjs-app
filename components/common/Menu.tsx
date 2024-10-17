6773aba16645ff4fe231fbb7ade4db42aea2ebf4
¨roniekun¨ <roniebenitez01@gmail.com>
2024-10-18 00:12:51 +0800

    update

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -15,19 +15,19 @@ const Menu = () => {
   return (
     <div style={{ fontFamily: "Neue Bit , Mori" }}>
       <button
-        className="capitalize selection:h-8 w-16 text-sm flex justify-center items-center bg-neutral-500 bg-opacity-15 overflow-hidden hover:border border-none border-neutral-500
+        className="capitalize h-8 w-16 text-sm flex justify-center items-center bg-neutral-500 bg-opacity-15 overflow-hidden hover:border border-none border-neutral-500
          rounded-md hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.7)] transition-shadow duration-300 z-10"
         onClick={handleClick}
       >
-        <div className="flex flex-col justify-center items-center relative">
+        <div className="flex flex-col justify-center items-center relative overflow-hidden">
           <AnimatePresence>
             {isToggleMenu && (
               <motion.p
-                initial={{ y: -20 }}
+                initial={{ y: -20, opacity: 0 }}
                 transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-                animate={{ y: 0 }}
-                exit={{ y: 20 }}
-                className="relative"
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 20, opacity: 0 }}
+                className="absolute"
               >
                 Close
               </motion.p>
@@ -36,11 +36,11 @@ const Menu = () => {
           <AnimatePresence initial={false}>
             {!isToggleMenu && (
               <motion.p
-                initial={{ y: -20 }}
+                initial={{ y: -20, opacity: 0 }}
                 transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-                animate={{ y: 0 }}
-                exit={{ y: 20 }}
-                className="relative"
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 20, opacity: 0 }}
+                className="absolute"
               >
                 Menu
               </motion.p>
