a2ef7d2d9c782c76507d191d0c123c11aefe53da
¨roniekun¨ <roniebenitez01@gmail.com>
2024-10-20 14:10:15 +0800

    update

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,11 +1,18 @@
 "use client";
 import Link from "next/link";
 import Container from "../lib/ui/container";
+import { useMenu } from "@/provider/context/MenuContext";
 
 const Logo = () => {
+  const { setToggleMenu } = useMenu();
+
   return (
     <Container className="p-0 my-1 font-normal text-lg leading-tight ">
-      <Link style={{ fontFamily: "Neue Bit, sans-serif" }} href="/">
+      <Link
+        onClick={() => setToggleMenu(false)}
+        style={{ fontFamily: "Neue Bit, sans-serif" }}
+        href="/"
+      >
         R&bull;Next
       </Link>
     </Container>
